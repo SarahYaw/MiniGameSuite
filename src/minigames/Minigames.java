8e192504447aa2def868d39ae264c5a122b61c33
sarahyaw <syaw@emich.edu>
2022-02-15 15:16:53 -0500

    rock paper scissors done
    finished logic for rock paper scissors

--- src/minigames/Minigames.java
+++ src/minigames/Minigames.java
@@ -1,7 +1,15 @@
 /*
  * @author SarahYaw
  * https://github.com/SarahYaw
- * just a fun little minigame suite to keep up to snuff on code :)
+ * I read a post online a few months back about how someone had an unusal 
+ * approach to these classic school problems with number generation and I 
+ * wanted to try it myself. Instead of the computer generating a number and 
+ * choosing between the plays (for rock paper scissors, for example), they 
+ * had the computer generate and choose if it won or lost and chose a play 
+ * based on that. The code is going to be longer and slightly more complicated 
+ * that way, but it changes the odds of the user winning a lot. I want to 
+ * see how many problems I can approach with that kind of logic. It's not 
+ * necessarily efficient or anything, but it is interesting :)
  */
 package minigames;
 
--- src/minigames/rockpaperscissors.java
+++ src/minigames/rockpaperscissors.java
@@ -1,24 +1,63 @@
 package minigames;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /*
  * @author SarahYaw
  * https://github.com/SarahYaw
- * just a fun little minigame suite to keep up to snuff on code :)
+ * weird way of doing rock paper scissors
  */
 public class rockpaperscissors {
+    private static final Random random = new Random();
     public static void play()
     {
         System.out.println("-------Welcome to Rock Paper Scissors-------");
         int round = 0, score = 0;
         Scanner keyIn = new Scanner(System.in);
-        boolean cont=true;
+        boolean cont=true, playerPoint;
         do {
+            round++;
+            //generate number and decide if player wins
+            int num = random.nextInt(11)+1;
+            playerPoint = num%2==0;
+            //fix score
+            if (playerPoint)
+                score++;
+            //take in user input
+            System.out.println("Choose:\n" + "1 -  Rock\n" + "2 - Paper\n" + "3 - Scissors");
+            //adapt output to user input and score
+            switch (keyIn.nextLine()) {
+                case "1" -> {
+                    if(!playerPoint)
+                        System.out.println("You chose: Rock\nComputer chose: Paper\nComputer wins");
+                    else
+                        System.out.println("You chose: Rock\nComputer chose: Scissors\nYou win!");
+                    System.out.println("Score: "+score+"/"+round+"\n");
+                }
+                case "2" -> {
+                    if (!playerPoint)
+                        System.out.println("You chose: Paper\nComputer chose: Scissors\nComputer wins");
+                    else
+                        System.out.println("You chose: Paper\nComputer chose: Rock\nYou win!");
+                    System.out.println("Score: "+score+"/"+round+"\n");
+                }
+                case "3" -> {
+                    if (!playerPoint)
+                        System.out.println("You chose: Scissors\nComputer chose: Rock\nComputer wins");
+                    else
+                        System.out.println("You chose: Scissors\nComputer chose: Paper\nYou win!");
+                    System.out.println("Score: "+score+"/"+round+"\n");
+                }
+                default -> System.out.println("Error");
+            }
+            //play again loop
             System.out.println("Would you like to play again? (y/n)");
             if(keyIn.next().equalsIgnoreCase("n"))
                 cont=false;
+            keyIn.nextLine();
         }while(cont);
+        System.out.println("--------------------------------------------");
     }
     
 }
